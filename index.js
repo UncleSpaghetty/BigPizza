068aea3ec954db53e08b0e408bb1d4f5b1592d56
Alessio Pellegrino <pellegrino-alessio@virgilio.it>
2021-05-30 12:40:40 +0200

    update

---
+++ index.js
@@ -0,0 +1,17 @@
+function initMap() {
+    // [START maps_add_map_instantiate_map]
+    const uluru = { lat: 40.014755, lng: 18.1564878 };
+
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 15,
+        center: uluru,
+    });
+    // [END maps_add_map_instantiate_map]
+    // [START maps_add_map_instantiate_marker]
+    // The marker
+    const marker = new google.maps.Marker({
+        position: uluru,
+        map: map,
+    });
+    // [END maps_add_map_instantiate_marker]
+}
